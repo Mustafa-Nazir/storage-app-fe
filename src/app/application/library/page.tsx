16ec320c6a6ad35570aad1d8d95428168b26f3c4
Mustafa Nazir <mustafa.nazir1112@gmail.com>
2024-02-20 20:07:04 +0300

    feat: notification icon is added

--- src/app/application/library/page.tsx
+++ src/app/application/library/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 import AccountBox from "@/components/ui/accountBox";
+import Notification from "@/components/ui/notification";
 
 export default function Library() {
     return (
         <div className="bg-main h-[100vh]">
             <div className="flex justify-end px-2 pt-2">
+                <Notification/>
                 <AccountBox name="Example" lastName="User" />
             </div>
         </div>
