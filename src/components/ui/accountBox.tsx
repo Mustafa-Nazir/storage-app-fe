166a85d63ec268f8364a1cb6f44c82e9b541b3a3
Mustafa Nazir <mustafa.nazir1112@gmail.com>
2024-02-25 21:46:53 +0300

    fix: mouse leave is added to account box and notification

--- src/components/ui/accountBox.tsx
+++ src/components/ui/accountBox.tsx
@@ -9,19 +9,26 @@ interface features {
 }
 
 const AccountBox = (features: features) => {
-    const [isClick, setIsClick] = useState(false);
+    const [isClicked, setIsClicked] = useState(false);
+
+    const toggle = () => {
+        setIsClicked(!isClicked);
+    }
+
+    const closeOnLeave = () => {
+        if(isClicked) setIsClicked(false);
+    }
 
     return (
-        <div className="relative">
-            <div className="z-20 relative bg-white w-fit h-[50px] rounded-xl flex items-center overflow-hidden border-2 border-gray-300">
+        <div className="relative" onMouseLeave={closeOnLeave}>
+            <div className="bg-white w-fit h-[50px] rounded-xl flex items-center overflow-hidden border-2 border-gray-300">
                 <div className="px-3 h-full flex items-center bg-main-light border-r-2 border-purple-light"><div><AccountIcon className="fill-white" /></div></div>
                 <div className="px-2 text-gray-900">{features.name} {features.lastName}</div>
-                <div className="px-2 cursor-pointer" onClick={() => setIsClick(!isClick)}><SettingsIcon className="fill-gray-500" /></div>
+                <div className="px-2 cursor-pointer" onClick={toggle}><SettingsIcon className="fill-gray-500" /></div>
             </div>
-            {isClick && (<div className="z-20 absolute right-4 top-14 rounded-lg border-2 border-gray-300 overflow-hidden">
+            {isClicked && (<div className="absolute right-4 top-13 rounded-lg border-2 border-gray-300 overflow-hidden">
                     <div className="bg-white w-full px-4 py-1 hover:bg-gray-100 cursor-pointer select-none">Çıkış Yap</div>
                 </div>)}
-            {isClick && (<BackgroundCover closeFunction={setIsClick}/>)}
         </div>
 
     );
--- src/components/ui/notification.tsx
+++ src/components/ui/notification.tsx
@@ -5,13 +5,20 @@ import BackgroundCover from "./backgroundCover";
 const Notification = () => {
     const [isClicked, setIsClicked] = useState(false);
 
+    const toggle = () => {
+        setIsClicked(!isClicked);
+    }
+
+    const closeOnLeave = () => {
+        if(isClicked) setIsClicked(false);
+    }
     return (
-        <div className="relative">
-            <div className="relative cursor-pointer h-full">
+        <div className="relative" onMouseLeave={closeOnLeave}>
+            <div className="relative cursor-pointer h-full" onClick={toggle}>
                 <div className="absolute w-[10px] h-[10px] rounded-full bg-red-dark right-5 top-3 border border-red-light"></div>
-                <div className="flex items-center justify-center h-full mr-3" onClick={() => setIsClicked(!isClicked)}><NotificationIcon className="fill-main-dark w-9 h-9" /></div>
+                <div className="flex items-center justify-center h-full mr-3"><NotificationIcon className="fill-main-dark w-9 h-9" /></div>
             </div>
-            {isClicked && (<div className="absolute right-4 top-14 rounded-lg border-2 border-gray-300 overflow-hidden">
+            {isClicked && (<div className="absolute right-4 top-10 rounded-lg border-2 border-gray-300 overflow-hidden">
                 <div className="bg-white w-full px-4 py-1 hover:bg-gray-100 cursor-pointer select-none whitespace-nowrap">...</div>
             </div>)}
         </div>
