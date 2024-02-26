3c73c101ec033477827545ce7adf540f771c4d7d
Mustafa Nazir <mustafa.nazir1112@gmail.com>
2024-02-26 17:41:52 +0300

    feat: card logo is added

--- src/components/ui/libraryCard.tsx
+++ src/components/ui/libraryCard.tsx
@@ -1,3 +1,5 @@
+import cadrLogo from "@/../../public/cardLogo.png";
+
 interface features{
     name:string,
 }
@@ -5,7 +7,11 @@ interface features{
 const LibraryCard = (features:features) => {
     return (
         <div className="flex flex-col items-center justify-center text-gray-500 h-[180px] w-[115px] bg-white rounded-2xl  border-2 border-gray-300 cursor-pointer select-none mx-3 my-2 overflow-hidden hover:ring-2 hover:ring-main-light hover:border-0">
-            <div className="h-[60%] w-full bg-[#d0d7fe] border-b-2 border-dashed border-white"></div>
+            <div className="h-[60%] w-full flex items-center justify-center bg-[#d0d7fe] border-b-2 border-dashed border-white">
+                <div className="w-[85%]">
+                    <img src={cadrLogo.src} alt="cardLogo" />
+                </div>
+            </div>
             <div className="h-[40%] text-md text-gray-700 flex items-center justify-center"><div className="text-center">{features.name}</div></div>
         </div>
     );
