e8b69a1cfa0b9b5f1dc25499e75b5092c71abc61
Mustafa Nazir <mustafa.nazir1112@gmail.com>
2024-04-04 17:00:22 +0300

    style: overflow is added

--- src/components/ui/container.tsx
+++ src/components/ui/container.tsx
@@ -1,6 +1,6 @@
 const Container = ({children}:any) => {
     return (
-        <div className="h-full m-5 bg-white rounded-2xl shadow-md p-4">
+        <div className="h-full m-5 bg-white rounded-2xl shadow-md p-4 overflow-y-auto">
             {children}
         </div>
     );
