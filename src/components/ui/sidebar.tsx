aee8aff3f8b95e46dd025a92c2a1f75eb8ba2d60
Mustafa Nazir <mustafa.nazir1112@gmail.com>
2024-04-04 23:58:45 +0300

    feat: libraries button is added to sidebar

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import SettingsIcon from "../icons/settingsIcon";
 import SidebarButton from "./sidebarButton";
 import TableRowsIcon from "../icons/tableRowsIcon";
+import LibrariesIcon from "../icons/librariesIcon";
 
 const Sidebar = () => {
     const [path,setPath] = useState("");
@@ -17,10 +18,15 @@ const Sidebar = () => {
     return (
         <div className="w-[200px] h-full bg-white mr-3 shadow-md">
             <div className="h-[90px] flex items-center justify-center my-3 font-bold text-3xl text-main-dark">LOGO</div>
-            <div>
-                <div className="px-3"><div className="w-full h-[1.5px] bg-gray-200 mb-5"></div></div>
-                <SidebarButton name="Duyurular" icon={TableRowsIcon} redirect={createPath("/announcements")}/>
-                <SidebarButton name="Ayarlar" icon={SettingsIcon} redirect={createPath("/settings")}/>
+            <div className="h-[80%] flex flex-col justify-between">
+                <div>
+                    <div className="px-3"><div className="w-full h-[1.5px] bg-gray-200 mb-5"></div></div>
+                    <SidebarButton name="Duyurular" icon={TableRowsIcon} redirect={createPath("/announcements")}/>
+                    <SidebarButton name="Ayarlar" icon={SettingsIcon} redirect={createPath("/settings")}/>
+                </div>
+                <div>
+                    <SidebarButton name="Kütüphaneler" icon={LibrariesIcon} redirect={"/application/library"}/>
+                </div>
             </div>
         </div>
     );
