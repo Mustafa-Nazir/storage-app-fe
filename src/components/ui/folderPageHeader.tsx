3d99404940f041433174549250a8ffc62b1e3d95
Mustafa Nazir <mustafa.nazir1112@gmail.com>
2024-04-06 16:59:24 +0300

    feat: redirect to documents page icon is added to folder page

--- src/components/ui/folderPageHeader.tsx
+++ src/components/ui/folderPageHeader.tsx
@@ -1,9 +1,10 @@
 import IUserLibraryDto from "@/models/library/IUserLibraryDto";
 import CreateNewFolderIcon from "../icons/createNewFolderIcon";
 import { useSelector } from "react-redux";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import FolderAddPopup from "./folderAddPopup";
 import { useState } from "react";
+import HomeIcon from "../icons/homeIcon";
 
 interface features{
     setFolders:Function
@@ -12,6 +13,7 @@ interface features{
 const FolderPageHeader = (features:features) => {
     const [isClicked , setIsClicked] = useState(false);
     const userLibraryInfo: IUserLibraryDto = useSelector((state: any) => state.userLibraryInfo);
+    const router = useRouter();
     const params = useParams();
     const libraryId = params.libraryId as string;
     const folderId = params.folderId as string;
@@ -22,10 +24,20 @@ const FolderPageHeader = (features:features) => {
         if (department) return department.name;
         return "Folder Name";
     }
+
+    const redirectToDocumentsPage = () => {
+        router.push(`/application/library/${libraryId}/documents`);
+    }
     return (
         <>
             <div className="flex justify-between">
-                <div className="flex items-center pl-2 pt-2 text-xl font-bold">{getFolderName()}</div>
+                <div className="flex">
+                    <div className="flex items-center pt-1">
+                        <div onClick={redirectToDocumentsPage} className="mx-1 cursor-pointer"><HomeIcon className="fill-main-dark hover:fill-main-light"/></div>
+                    </div>
+                    <div className="flex items-center pl-2 pt-2 text-xl font-bold ml-2">{getFolderName()}</div>
+                </div>
+                
                 <div onClick={()=>{setIsClicked(true)}} className="flex items-center pt-2 cursor-pointer"><CreateNewFolderIcon className="w-8 h-8 fill-main-dark hover:fill-main-light" /></div>
             </div>
 
